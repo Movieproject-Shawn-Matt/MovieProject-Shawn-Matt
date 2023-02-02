a0011762651c48c0648273bdc855ebb0a946fa81
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-02-02 14:08:31 -0600

    added ability to add movies

--- js/MovieProject.js
+++ js/MovieProject.js
@@ -15,6 +15,34 @@ $.get("https://light-quilted-boar.glitch.me/movies").done(function (data) {
 
 
 
+const titleTextBox = $("#title-text-box");
+const ratingTextBox = $('#rating-text-box')
+const genreTextBox = $('#genre-text-box')
+const summaryTextBox = $('#summary-text-box')
+const button = $("#button");
+
+button.click(function() {
+    const titleValue = titleTextBox.val();
+    const ratingValue = ratingTextBox.val();
+    const genreValue = genreTextBox.val();
+    const summaryValue = summaryTextBox.val();
+
+
+    $.ajax({
+        url: "https://light-quilted-boar.glitch.me/movies",
+        type: "POST",
+        data: JSON.stringify({ title: titleValue, rating: ratingValue, genre: genreValue, summary: summaryValue }),
+        contentType: "application/json",
+        success: function(data) {
+            console.log(data);
+        },
+        error: function(error) {
+            console.error(error);
+        }
+    });
+});
+
+
 
 
 
