6e8f6771d5a023cf9e7f7b895e2a868a89250842
Matt Guardiola <matt.g.guardiola@gmail.com>
2023-02-02 16:44:44 -0600

    add and delete

--- js/MovieProject.js
+++ js/MovieProject.js
@@ -5,19 +5,38 @@ function addNewCards () {
         console.log(data);
         let index = 0;
         $(".weekly-forecast").html("");
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < data.length; i++) {
             $("#movie-cards").append(
                 '<div class="movie-cards">' +
-                '<button type="button" class="delete-btn btn btn-outline-danger w-15 p-1">' + 'X' + '</button>' +
+                '<button type="button" data-id="'+ data[i].id +'" class="delete-btn btn btn-outline-danger w-15 p-1">' + 'X' + '</button>' +
                 '<h5 id="title" class="card-title">' + JSON.stringify(data[i].title) + '</h5>' +
                 '<div id="rating" class="card-body">' + JSON.stringify(data[i].rating) + '</div>' +
                 '<div id="rating" class="card-body">' + JSON.stringify(data[i].genre) + '</div>' +
                 '<div id="rating" class="card-body">' + JSON.stringify(data[i].summary) + '</div>' +
-                '<div id="rating" class="card-body">' + JSON.stringify(data[i].id) + '</div>' +
+                '<div id="id" class="card-body">' + JSON.stringify(data[i].id) + '</div>' +
                 '</div>'
             )
             index += 8
         }
+        const deleteButton = $(".delete-btn");
+
+        deleteButton.click(function() {
+            const id = $(this).attr('data-id');
+            alert(id)
+
+            $.ajax({
+                url: `https://light-quilted-boar.glitch.me/movies/${id}`,
+                type: "DELETE",
+                success: function(data) {
+                    console.log(data);
+                    addNewCards()
+                },
+                error: function(error) {
+                    console.error("There was a problem with the delete operation:", error);
+                }
+            });
+        });
+
     })
 }
 addNewCards()
@@ -59,8 +78,3 @@ button.click(function() {
 
 
 
-//reload cards on button press
-$("#button").click(function(){
-    $('#movie-cards').load('.movie-cards')
-    alert('Reloaded')
-});
